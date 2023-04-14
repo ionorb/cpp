4b933f5cd03a41fe57ac665c3c894391fd751e63
Yoel RIDGWAY-LOPEZ <yoelridgway@gmail.com>
2023-04-14 20:23:48 +0200

    finished my fixed point class and finished converting to floats and ints and vice versa

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -6,12 +6,13 @@
 /*   By: yridgway <yridgway@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 20:28:01 by yridgway          #+#    #+#             */
-/*   Updated: 2023/04/14 18:57:10 by yridgway         ###   ########.fr       */
+/*   Updated: 2023/04/14 20:17:24 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 Fixed::Fixed(void)
 {
@@ -39,12 +40,40 @@ Fixed::~Fixed(void)
 
 int		Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called\n";
+	// std::cout << "getRawBits member function called\n";
 	return (this->rawbits);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called\n";
+	// std::cout << "setRawBits member function called\n";
 	this->rawbits = raw;
+}
+
+Fixed::Fixed(const int integer)
+{
+	this->rawbits = integer << this->fbits;
+	std::cout << "Int constructor called\n";
+}
+
+Fixed::Fixed(const float floating)
+{
+	std::cout << "Float constructor called\n";
+	this->rawbits = (int)roundf(floating * (1 << this->fbits));
+}
+		
+float	Fixed::toFloat(void) const
+{
+	return ((float)this->rawbits / (float)(1 << this->fbits));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (this->rawbits >> 8);
+}
+
+std::ostream&	operator << (std::ostream& out, const Fixed& fixed)
+{
+	out << fixed.toFloat();
+	return (out);
 }
--- module_02/ex01/Fixed.hpp
+++ module_02/ex01/Fixed.hpp
@@ -6,25 +6,33 @@
 /*   By: yridgway <yridgway@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 20:28:05 by yridgway          #+#    #+#             */
-/*   Updated: 2023/04/14 18:57:00 by yridgway         ###   ########.fr       */
+/*   Updated: 2023/04/14 20:12:32 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
+#include <iostream>
+
 class	Fixed
 {
 	private:
 		int					rawbits;
-		static const int	bits = 8;
+		static const int	fbits = 8;
 	
 	public:
 		Fixed();
 		Fixed(const Fixed& copy);
-		Fixed&	operator= (const Fixed& fixed);
+		Fixed(const int integer);
+		Fixed(const float floating);
+		Fixed&	operator = (const Fixed& fixed);
 		~Fixed();
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 };
+
+std::ostream&	operator << (std::ostream& out, const Fixed& fixed);
 
 #endif
