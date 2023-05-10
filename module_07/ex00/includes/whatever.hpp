cf7a73de7b4e86f1ca2162696fda503bd81067b4
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-05-10 16:35:23 +0000

    fixed the swap function by parsing references

--- module_07/ex00/includes/whatever.hpp
+++ module_07/ex00/includes/whatever.hpp
@@ -6,18 +6,18 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:42:13 by codespace         #+#    #+#             */
-/*   Updated: 2023/05/10 14:54:08 by codespace        ###   ########.fr       */
+/*   Updated: 2023/05/10 16:17:52 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
-template <typename T> void swap(T first, T second)
+template <typename T> void swap(T& first, T& second)
 {
-	T temp = T(first);
-	first = T(second);
-	second = T(temp);
+	T temp = first;
+	first = second;
+	second = temp;
 }
 
 template <typename T> T min(T first, T second)
