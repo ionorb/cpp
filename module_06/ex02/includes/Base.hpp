a291ade5aca223d2ffd9ef3dfcf32cdc2f6ec9cd
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-05-10 12:17:17 +0000

    initial commit

---
+++ module_06/ex02/includes/Base.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 12:10:41 by codespace         #+#    #+#             */
+/*   Updated: 2023/05/10 12:16:46 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+# define BASE_HPP
+#include <limits>
+#include <iostream>
+#include <string>
+#include <stdint.h>
+
+class Base
+{
+	public:
+		virtual ~Base();
+};
+
+#endif
