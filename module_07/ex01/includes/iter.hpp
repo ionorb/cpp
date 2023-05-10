dcb098fbfafbdf807d43fc7a5026169dc79af224
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-05-10 18:21:07 +0000

    added some cool functions to test

--- module_07/ex01/includes/iter.hpp
+++ module_07/ex01/includes/iter.hpp
@@ -6,13 +6,14 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:41:57 by codespace         #+#    #+#             */
-/*   Updated: 2023/05/10 17:13:52 by codespace        ###   ########.fr       */
+/*   Updated: 2023/05/10 18:07:40 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 #include <iostream>
+#include <cstdlib>
 
 template<typename T> void iter(T* arr, unsigned long size, void (*func)(T&))
 {
--- module_07/ex01/sources/main.cpp
+++ module_07/ex01/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 17:02:36 by codespace         #+#    #+#             */
-/*   Updated: 2023/05/10 17:16:36 by codespace        ###   ########.fr       */
+/*   Updated: 2023/05/10 18:20:02 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,52 @@ void	addOne(int& i)
 	i++;
 }
 
+void	capitalize(std::string& i)
+{
+	for (std::size_t j = 0; j < i.size(); j++)
+	{
+		if (j == 0 || i[j - 1] == ' ')
+			i[j] = toupper(i[j]);
+	}
+}
+
+void	scramble(std::string& i)
+{
+	std::srand((unsigned)time(NULL) * (unsigned)clock());
+	char		tmp;
+	std::size_t	index1, index2;
+
+	for (std::size_t j = 0; j < i.size(); j++)
+	{
+		index1 = rand() % i.size();
+		index2 = rand() % i.size();
+		tmp = i[index1];
+		i[index1] = i[index2];
+		i[index2] = tmp;
+	}
+}
+
 int	main(void)
 {
-	int i[100];
+	{
+		int i[100];
 
-	for (int j = 0; j < 100; j++)
-		i[j] = 100 - j;
-	::iter(i, 100, *addOne);
-	for (int j = 0; j < 100; j++)
-		std::cout << i[j] << ", ";
-	std::cout << std::endl;
+		for (int j = 0; j < 100; j++)
+			i[j] = 100 - j;
+		::iter(i, 100, *addOne);
+		for (int j = 0; j < 100; j++)
+			std::cout << i[j] << ", ";
+		std::cout << std::endl;
+	}
+	{
+		std::string	i[100];
+		
+		for (int j = 0; j < 100; j++)
+			i[j] = "banana pancakes";
+		::iter(i, 100, *scramble);
+		for (int j = 0; j < 100; j++)
+			std::cout << i[j] << ", ";
+		std::cout << std::endl;
+	}
 	return (0);
 }
