a2e2cd0c77284760d775b453ea2a737204ff403d
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-05-10 11:26:49 +0000

    initial commit

---
+++ module_06/ex01/includes/Serializer.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 11:17:46 by codespace         #+#    #+#             */
+/*   Updated: 2023/05/10 11:26:33 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+#include <limits>
+#include <iostream>
+#include <string>
+
+class Serializer
+{
+	private:
+		Serializer();
+
+	public:
+		static char		character;
+};
+
+#endif
