7f3361f804fe62195e26894b88b2bb602be0a788
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-05-10 18:31:46 +0000

    added hard test from correction

--- module_07/ex00/sources/main.cpp
+++ module_07/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:46:48 by codespace         #+#    #+#             */
-/*   Updated: 2023/05/10 16:16:22 by codespace        ###   ########.fr       */
+/*   Updated: 2023/05/10 18:31:08 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,48 @@ int	main( void )
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 }
+
+// class Awesome
+// {
+// 	public:
+// 		Awesome(void) : _n(0) {}
+// 		Awesome( int n ) : _n( n ) {}
+// 		Awesome & operator= (Awesome & a) {
+// 			_n = a._n; return *this;
+// 		}
+// 		bool operator==( Awesome const & rhs ) const {
+// 			return (this->_n == rhs._n);
+// 		}
+// 		bool operator!=( Awesome const & rhs ) const {
+// 			return (this->_n != rhs._n);
+// 		}
+// 		bool operator>( Awesome const & rhs ) const {
+// 			return (this->_n > rhs._n);
+// 		}
+// 		bool operator<( Awesome const & rhs ) const {
+// 			return (this->_n < rhs._n);
+// 		}
+// 		bool operator>=( Awesome const & rhs ) const {
+// 			return (this->_n >= rhs._n);
+// 		}
+// 		bool operator<=( Awesome const & rhs ) const {
+// 			return (this->_n <= rhs._n);
+// 		}
+// 		int get_n() const{
+// 			return _n;
+// 		}
+// 		private: int _n;
+// };
+
+// std::ostream & operator<<(std::ostream & o, const Awesome &a) {
+// 	o << a.get_n(); return o;
+// }
+
+// int main(void)
+// {
+// 	Awesome a(4), b(2); swap(a, b);
+// 	std::cout << a << " " << b << std::endl;
+// 	std::cout << max(a, b) << std::endl;
+// 	std::cout << min(a, b) << std::endl;
+// 	return (0);
+// }
