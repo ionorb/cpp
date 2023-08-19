15bea114b36f85aa02aa2b222486969b3360d7ba
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-08-19 18:56:01 +0000

    getting bulk of functions layed out

--- module_09/ex00/includes/BitcoinExchange.hpp
+++ module_09/ex00/includes/BitcoinExchange.hpp
@@ -6,6 +6,11 @@
 #include <string>
 #include <fstream>
 #include <stdlib.h>
+#include <iomanip>
+#include <algorithm>
+
+#define SSTR( x ) static_cast< std::ostringstream & >( \
+        ( std::ostringstream() << std::dec << x ) ).str()
 
 class BitcoinExchange
 {
@@ -19,19 +24,25 @@ class BitcoinExchange
 		//methods
 
 		//getters
-		std::map<std::string, float>	getData() const;
+		std::map<std::string, double>	getData() const;
 
 		//setters
 
 	private:
 		//attributes
-		std::map<std::string, float>	_data;
+		std::map<std::string, double>	_data;
 		std::string						_rawData;
 		std::string						_input;
 
 		//methods
 		std::string						readFile(std::string path) const;
-		std::map<std::string, float>	parseData(std::string rawData) const;
+		std::map<std::string, double>	parseData(std::string rawData) const;
+		std::string						removeWhite(std::string str) const;
+		void							processInput(std::string input) const;
+		std::string						calculateLine(std::string date, std::string value) const;
+		void							checkDate(std::string date) const;
+
+
 
 };
 
--- module_09/ex00/sources/BitcoinExchange.cpp
+++ module_09/ex00/sources/BitcoinExchange.cpp
@@ -6,6 +6,57 @@ BitcoinExchange::BitcoinExchange(std::string input_path)
 	this->_input = this->readFile(input_path);
 	this->_rawData = this->readFile("data.csv");
 	this->_data = this->parseData(this->_rawData);
+	this->processInput(this->_input);
+}
+
+std::string	BitcoinExchange::calculateLine(std::string date, std::string value) const
+{
+	std::map<std::string, double>	map(this->_data);
+	std::string					ret;
+
+	// std::cout << "calc line\n";
+	this->checkDate(date);
+	if (map.count(date))
+		ret = date + " => " + value + " = " + SSTR(map[date] * atof(value.c_str())) + "\n";
+	return (ret);
+}
+
+void	BitcoinExchange::checkDate(std::string date) const
+{
+	(void)date;
+}
+
+void	BitcoinExchange::processInput(std::string input) const
+{
+	std::istringstream	in(input);
+	std::string			line, date, value;
+
+	std::getline(in, date);
+	while(std::getline(in, line))
+	{
+		try
+		{
+			if (line.find('|') == line.npos)
+				throw std::runtime_error("bad input");
+			line = this->removeWhite(line);
+			date = line.substr(0, line.find('|'));
+			value = line.substr(line.find('|') + 1);
+			// std::cout << "\'" << date << "\' | \'" << value << "\'" << std::endl;
+			std::cout << this->calculateLine(date, value);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Error: " << e.what() << '\n';
+		}
+	}
+	// std::cout << "map: " << this->_data["2022-03-10"] << std::endl;
+}
+
+std::string	BitcoinExchange::removeWhite(std::string str) const
+{
+	std::string	nows = str;
+	nows.erase(remove_if(nows.begin(), nows.end(), isspace), nows.end());
+	return (nows);
 }
 
 BitcoinExchange::~BitcoinExchange() {}
@@ -24,7 +75,7 @@ BitcoinExchange&	BitcoinExchange::operator = (const BitcoinExchange& copy)
 	return (*this);
 }
 
-std::map<std::string, float>	BitcoinExchange::getData() const
+std::map<std::string, double>	BitcoinExchange::getData() const
 {
 	return (this->_data);
 }
@@ -41,25 +92,25 @@ std::string	BitcoinExchange::readFile(std::string path) const
 	return (buffer.str());
 }
 
-std::map<std::string, float>	BitcoinExchange::parseData(std::string rawData) const
+std::map<std::string, double>	BitcoinExchange::parseData(std::string rawData) const
 {
-	typedef std::map<std::string, float> map;
+	typedef std::map<std::string, double> map;
 	map								data;
 	std::string						key, val;
 	std::istringstream				raw(rawData);
 	std::string						first;
 
-	getline(raw, key);
 	// while (getline(raw, line, ','))
 	// {
 	// 	std::istringstream	buf(line);
 	// 	// getline(raw, first, ',');
 	// 	std::cout << line << ", " << first << std::endl;
 	// }
+	getline(raw, key);
 	while(std::getline(std::getline(raw, key, ',') >> std::ws, val))
 		data[key] = atof(val.c_str());
-	for (map::iterator it = data.begin(); it != data.end(); it++)
-		std::cout << "key: " << it->first << " val: " << it->second << std::endl;
+	// for (map::iterator it = data.begin(); it != data.end(); it++)
+	// 	std::cout << it->first << "," << std::setprecision(10) << it->second << std::endl;
 		
 	return (data);
 }
