9be3e276fb50e12b8344e0dd79575c0a43d278fe
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-08-25 15:25:34 +0000

    changed function to use algorithm library

--- module_08/ex00/includes/easyfind.hpp
+++ module_08/ex00/includes/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:42:13 by codespace         #+#    #+#             */
-/*   Updated: 2023/08/19 11:28:14 by codespace        ###   ########.fr       */
+/*   Updated: 2023/08/25 15:23:08 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,20 @@
 # define EASYFIND_HPP
 #include <cstddef>
 #include <iostream>
+#include <algorithm>
 
 
 template <typename T> void easyfind(T container, int number)
 {
-	size_t	i = 0;
-	for (typename T::iterator it = container.begin(); it != container.end(); it++)
-	{
-		if (*it == number)
-		{
-			std::cout << "Integer \'" << number \
-			<< "\' was found in the container at index: " \
-			<< i << std::endl;
-			return ;
-		}
-		i++;
+	typename T::iterator it = std::find(container.begin(), container.end(), number);
+	if (it != container.end())
+	{		
+		std::cout << "Integer \'" << number \
+				  << "\' was found in the container at index: " \
+				  << std::distance(container.begin(), it) << std::endl;
 	}
-	std::cout << "Integer \'" << number << "\' was not found in the container" << std::endl;
+	else
+		std::cout << "Integer \'" << number << "\' was not found in the container" << std::endl;
 }
 
 #endif
--- module_08/ex00/sources/main.cpp
+++ module_08/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:46:48 by codespace         #+#    #+#             */
-/*   Updated: 2023/08/19 11:27:01 by codespace        ###   ########.fr       */
+/*   Updated: 2023/08/25 15:20:49 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,29 @@
 #include <list>
 #include <deque>
 
+# define N 5
+
 int	main(int ac, char **av) 
 {
 	int tofind = 1;
+
 	if (ac == 2)
 		tofind = atoi(av[1]);
-	int	n = 5;
-	int	arr[n] = {1, 3, 4, 2, 5};
-	std::vector<int> vec(arr, arr + n);
-	std::list<int> lst(arr, arr + n);
-	std::deque<int> deq(arr, arr + n);
+
+	int	arr[N] = {1, 3, 4, 2, 5};
+
+	std::cout << "array: ";
+	for (size_t i = 0; i < N; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+
+	std::vector<int> vec(arr, arr + N);
+	std::list<int> lst(arr, arr + N);
+	std::deque<int> deq(arr, arr + N);
 
 	::easyfind(vec, tofind);
 	::easyfind(lst, tofind);
 	::easyfind(deq, tofind);
+
 	return 0;
 }
